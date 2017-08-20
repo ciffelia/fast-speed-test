2045ffe42d593ac408018331cc86697804d25c85
prince0203 <mc.prince.0203@gmail.com>
2017-08-20 12:04:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# fast-speed-test
+[![Build Status](https://travis-ci.org/prince0203/fast-speed-test.svg?branch=master)](https://travis-ci.org/prince0203/fast-speed-test)
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)  
+Fast.com Internet Speed Test
+
+## Usage
+
+```bash
+$ yarn
+$ node index.js
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+
+  console.log('Chrome launched');
+  
+  const page = await browser.newPage();
+  await page.goto('https://fast.com/');
+
+  console.log('Fast.com loaded');
+
+  await page.waitFor('#speed-value.succeeded');
+  
+  const currentTime = Math.floor(new Date().getTime() / 1000);
+
+  const speedValue = await page.evaluate(() => {
+    const speedValueElem = document.getElementById('speed-value');
+    return Promise.resolve(speedValueElem.innerText);
+  });
+
+  const speedUnits = await page.evaluate(() => {
+    const speedUnitsElem = document.getElementById('speed-units');
+    return Promise.resolve(speedUnitsElem.innerText);
+  });
+
+  console.log(speedValue + speedUnits);
+
+  fs.appendFileSync('result.csv', currentTime + ',' + speedValue + ',' + speedUnits + '\n');
+
+  browser.close();
+})().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
+
