4db118c38180c65abd4d4180c6932bd3f2b193cb
Ciffelia <mc.prince.0203@gmail.com>
2020-03-04 14:45:14 +0900

    Introduce JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-const fs = require('fs');
+const fs = require('fs')
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const args = process.argv.slice(2);
+  const args = process.argv.slice(2)
   const executablePath =
     args.includes('--chrome-executable')
-    ? args[args.indexOf('--chrome-executable') + 1]
-    : undefined;
-  
-  const browser = await puppeteer.launch({executablePath});
+      ? args[args.indexOf('--chrome-executable') + 1]
+      : undefined
 
-  console.log('Chrome launched');
-  
-  const page = await browser.newPage();
-  await page.goto('https://fast.com/');
+  const browser = await puppeteer.launch({ executablePath })
 
-  console.log('Fast.com loaded');
+  console.log('Chrome launched')
 
-  await page.waitFor('#speed-value.succeeded');
-  
-  const currentTime = Math.floor(new Date().getTime() / 1000);
+  const page = await browser.newPage()
+  await page.goto('https://fast.com/')
+
+  console.log('Fast.com loaded')
+
+  await page.waitFor('#speed-value.succeeded')
+
+  const currentTime = Math.floor(new Date().getTime() / 1000)
 
   const speedValue = await page.evaluate(() => {
-    const speedValueElem = document.getElementById('speed-value');
-    return Promise.resolve(speedValueElem.innerText);
-  });
+    const speedValueElem = document.getElementById('speed-value')
+    return Promise.resolve(speedValueElem.innerText)
+  })
 
   const speedUnits = await page.evaluate(() => {
-    const speedUnitsElem = document.getElementById('speed-units');
-    return Promise.resolve(speedUnitsElem.innerText);
-  });
+    const speedUnitsElem = document.getElementById('speed-units')
+    return Promise.resolve(speedUnitsElem.innerText)
+  })
 
-  console.log(speedValue + speedUnits);
+  console.log(speedValue + speedUnits)
 
-  fs.appendFileSync('result.csv', currentTime + ',' + speedValue + ',' + speedUnits + '\n');
+  fs.appendFileSync('result.csv', currentTime + ',' + speedValue + ',' + speedUnits + '\n')
 
-  browser.close();
+  browser.close()
 })().catch(err => {
-  console.error(err);
-  process.exit(1);
-});
-
+  console.error(err)
+  process.exit(1)
+})
